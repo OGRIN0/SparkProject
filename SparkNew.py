9ba088f476c4a81c428bbc958661adff752e60a8
AMARNATH <142997781+OGRIN0@users.noreply.github.com>
2024-11-14 00:29:25 +0530

    Update SparkNew.py

--- SparkNew.py
+++ SparkNew.py
@@ -1,7 +1,13 @@
+from sqlite3.dbapi2 import Timestamp
+
 import  findspark
+from pyspark.sql.types import TimestampType
+
 findspark.init()
 
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import to_date, from_unixtime, unix_timestamp
+from pyspark.sql.types import TimestampType
 
 
 spark = (SparkSession.builder.master("local[*]")
@@ -23,6 +29,21 @@ valid_df = input_df.filter("'Temperature (c)' is not null and "
                            "'Pressure' is not null")
 
 
-valid_df.write.format('csv').mode('overwrite').option('header', True).save('out_valid')
+valid_df = valid_df.withColumnRenamed("_c0", "DateTime") \
+    .withColumnRenamed("_c1", "Temperature") \
+    .withColumnRenamed("_c2", "Humidity") \
+    .withColumnRenamed("_c3", "Wind_Speed") \
+    .withColumnRenamed("_c4", "Pressure")
+
+
+
+valid_date_df = valid_df.withColumn("Date",
+                                    to_date(unix_timestamp("DateTime", "MM/dd/yy hh:mm a")
+                                            .cast("timestamp")))
+
+valid_date_df.write.option("header","true")\
+    .mode("overwrite")\
+    .partitionBy("Date")\
+    .csv(out_valid)
 
 
